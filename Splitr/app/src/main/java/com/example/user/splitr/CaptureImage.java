57ceb3c3c067dcdd135b74e8c1c10a7eb5341f70
davidmorrison1981 <david.morrison@geminises.com>
2016-10-30 15:00:01 +0000

    pages created

---
+++ Splitr/app/src/main/java/com/example/user/splitr/CaptureImage.java
@@ -0,0 +1,22 @@
+package com.example.user.splitr;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+
+/**
+ * Created by user on 29/10/2016.
+ */
+
+public class CaptureImage extends AppCompatActivity {
+
+    Button mCaptureImage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.capture_image);
+
+//        TODO add camera image capture setOnClick
+    }
+}
--- Splitr/app/src/main/java/com/example/user/splitr/MainActivity.java
+++
@@ -1,7 +0,0 @@
-package com.example.user.splitr;
-
-/**
- * Created by user on 28/10/2016.
- */
-public class MainActivity {
-}
